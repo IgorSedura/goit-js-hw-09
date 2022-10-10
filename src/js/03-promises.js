7a35190ef319d4e66f664527e0c090c6aa063a5b
Igor Sedura <igorsedura@gmail.com>
2022-10-10 23:24:28 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,43 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const form = document.querySelector('.form');
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const {
+    elements: { delay, step, amount },
+  } = event.currentTarget;
+
+  let totalDelay = Number(delay.value) - Number(step.value);
+  for (let i = 0; i < amount.value; i += 1) {
+    const position = i + 1;
+    totalDelay = totalDelay + Number(step.value);
+    createPromise(position, totalDelay);
   }
+}
+
+function createPromise(position, totalDelay) {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, totalDelay });
+      } else {
+        reject({ position, totalDelay });
+      }
+    }, totalDelay);
+  });
+  promise
+    .then(({ position, totalDelay }) => {
+      Notify.success(`✅ Fulfilled promise ${position} in ${totalDelay}ms`, {
+        timeout: 4000,
+      });
+    })
+    .catch(({ position, totalDelay }) => {
+      Notify.failure(`❌ Rejected promise ${position} in ${totalDelay}ms`, {
+        timeout: 4000,
+      });
+    });
 }
