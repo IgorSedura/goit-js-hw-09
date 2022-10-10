b942afb0893b3ef2e8df781a3adc13f2c78debe5
IgorSedura <40833741+IgorSedura@users.noreply.github.com>
2022-10-10 14:41:27 +0000

    Deploying to gh-pages from @ IgorSedura/goit-js-hw-09@6a7d7ef0bd76ea69efc9259add26279694133555 🚀

---
+++ 01-color-switcher.bcf994e1.js
@@ -0,0 +1,2 @@
+const t={start:document.querySelector("[data-start]"),stop:document.querySelector("[data-stop]"),body:document.querySelector("body")};console.log(t),t.stop.disabled=!0,t.start.addEventListener("click",(function(){e=setInterval((()=>{t.body.style.background=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),t.start.disabled=!0,t.stop.disabled=!1})),t.stop.addEventListener("click",(function(){clearTimeout(e),t.start.disabled=!1,t.stop.disabled=!0}));let e=null;
+//# sourceMappingURL=01-color-switcher.bcf994e1.js.map
